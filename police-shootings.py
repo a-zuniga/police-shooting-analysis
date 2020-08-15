a86cb3d03411f0a52cb533d0c1d171f851151e8e
Alejandro Zuniga <azunigas@gmu.edu>
2020-08-14 21:38:21 -0400

    Fixed unarmed victim computation

--- police-shootings.py
+++ police-shootings.py
@@ -6,6 +6,8 @@ with open('fatal-police-shooting-data.csv', 'r') as data_set:
     total_victims = 0
     under_age_victims = 0
     under_age_victims_list = []
+    unarmed_victims = 0
+    unarmed_victims_list = []
 
     '''
     header = [Id, Name, Date, Manner of death, Armed, Age, Gender, 
@@ -22,16 +24,27 @@ with open('fatal-police-shooting-data.csv', 'r') as data_set:
         race = row[7]
         state = row[9]
 
+        '''
+        Validates age of victim and adds to list if underage
+        '''
         if(age != ''):
             age = int(age)
-            if(age <18):
+            if(age < 18):
                 under_age_victims += 1
                 victim = dict({"name": name, "age": age, "date": date,
-                            "armed": armed, "race": race, "state": state})
+                               "armed": armed, "race": race, "state": state})
                 under_age_victims_list.append(victim)
+        if(armed == 'unarmed' or armed == "toy weapon"):
+            unarmed_victims += 1
+            unarmed_victim = dict({"name": name, "age": age, "date": date,
+                                   "armed": armed, "race": race, "state": state})
+            unarmed_victims_list.append(unarmed_victim)
         total_victims += 1
-    
-    for victim in range(len(under_age_victims_list)):
-        print(under_age_victims_list[victim])
+
     print(f'Total victims: {total_victims}')
     print(f'Underage victims: {under_age_victims}')
+    print(f'Unarmed victims: {unarmed_victims}')
+
+    unarmed_victim_percentage = (unarmed_victims / total_victims) * 100
+    unarmed_victim_percentage = round(unarmed_victim_percentage,2)
+    print(f'Unarmed victims percentage: {unarmed_victim_percentage}%')
