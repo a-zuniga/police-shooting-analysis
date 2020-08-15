41be9dcb4fcd966dc7857ab0831f5c6d0cc69c63
Alejandro Zuniga <azunigas@gmu.edu>
2020-08-14 21:16:00 -0400

    Fixed underaged victim computation

--- police-shootings.py
+++ police-shootings.py
@@ -1,42 +1,37 @@
 import csv
 
-path = "C:/Users/ajz22/Documents/Summer2020/case-study/fatal-police-shooting-data.csv"
-file = open(path, newline='')
-reader = csv.reader(file)
+with open('fatal-police-shooting-data.csv', 'r') as data_set:
 
-header = next(reader) #File Header
+    reader = csv.reader(data_set)
+    total_victims = 0
+    under_age_victims = 0
+    under_age_victims_list = []
 
-unarmed_victims = {}
-unarmed_victim_count = 0
+    '''
+    header = [Id, Name, Date, Manner of death, Armed, Age, Gender, 
+        Race, City, State, Sign of Menral Illness, Flee, Body Camera]
+    '''
+    header = next(reader)
 
+    for row in reader:
 
-'''
-header = [Id, Name, Date, Manner of death, Armed, Age, Gender, 
-       Race, City, State, Sign of Menral Illness, Flee, Body Camera]
+        name = row[1]
+        age = row[5]
+        date = row[2]
+        armed = row[4]
+        race = row[7]
+        state = row[9]
 
-Q1) How many unarmed shootings victims are there in the data set?
-
-Answer: 
-'''
-
-for row in reader:
-
-    name = row[1]
-    age = row[5]
-    date = row[2]
-    armed = row[4]
-    race = row[7]
-    state = row[9]
-
-    print(row)
-
-    if(armed == "unarmed"):
-        victim = dict({"name":name, "age": age, "date": date, "armed": armed, "race": race, "state": state})
-        unarmed_victims.update(victim)
+        if(age != ''):
+            age = int(age)
+            if(age <18):
+                under_age_victims += 1
+                victim = dict({"name": name, "age": age, "date": date,
+                            "armed": armed, "race": race, "state": state})
+                under_age_victims_list.append(victim)
+        total_victims += 1
     
-
-#for victim in unarmed_victims.values():
-    #print(victim)
-
-unarmed_victim_count = len(unarmed_victims)
-print(f"Total unarmed victims: {unarmed_victim_count}")
+    for victim in range(len(under_age_victims_list)):
+        print(under_age_victims_list[victim])
+    print(f'Total victims: {total_victims}')
+    print(f'Underage victims: {under_age_victims}')
